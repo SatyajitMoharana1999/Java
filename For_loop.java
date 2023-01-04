2f75e0eeb8dd9d42568cd494a12ee10056c2fa76
Satyajit Moharana <117591813+SatyajitMoharana1999@users.noreply.github.com>
2023-01-04 08:30:13 +0530

    Update For_loop.java

--- For_loop.java
+++ For_loop.java
@@ -3,7 +3,7 @@ public class For_loop{
   public static void main(String args[]){
     Scanner sc = new Scanner(System.in);
     System.out.println("enter how many times the loop run");
-    int n=sc.nextInt;
+    int n=sc.nextInt();
     for(int i=1;i<=n;i++){
       System.out.print(i + " ");
     }  
