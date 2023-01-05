d56ac97eb14eb79db1203be1b143007fcc221856
Satyajit Moharana <117591813+SatyajitMoharana1999@users.noreply.github.com>
2023-01-05 19:40:52 +0530

    Create Do_while_loop.java

---
+++ Do_while_loop.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Do_while_loop{
+  public static void main(String args[]){
+    Scanner sc =new Scanner(System.in);
+    System.out.println("Enter how many times you want to print 'hi'");
+    int n=sc.nextInt();
+    int i=1;
+    do{
+      System.out.println("Hi");
+    }while(i<=n);
+  }
+}
