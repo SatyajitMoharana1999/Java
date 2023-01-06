5988f6b1325a979fcbebbdf74430f916fbb47363
Satyajit Moharana <117591813+SatyajitMoharana1999@users.noreply.github.com>
2023-01-06 12:45:40 +0530

    Create EvenAndOdd.java

---
+++ EvenAndOdd.java
@@ -0,0 +1,18 @@
+public class EvenAndOdd{
+  public static void main(String args[]){
+  int user_input;
+  int even;
+  int odd;
+  Scanner sc=new Scanner(System.in);
+  System.out.printf("enter range");
+  user_input=sc.nextInt();
+  System.out.printf("Even numbers :");
+  for(int i=2;i<=user_input;i=i+2){
+     System.out.printf("%d ",i);
+  }
+  System.out.printf("\nOdd numbers :");
+  for(int i=1;i<=user_input;i=i+2){
+     System out.printf("%d ",i);
+  }
+  }
+}
