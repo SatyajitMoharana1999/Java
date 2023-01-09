4ee7d53a83e9169b9ef556dacb2b008a432f0bad
Satyajit Moharana <117591813+SatyajitMoharana1999@users.noreply.github.com>
2023-01-09 09:21:43 +0530

    Create FindGretterNumber.java

---
+++ FindGretterNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class FindGretterNumber{
+  public static void main(String args[]){
+    int input;
+    int sum;
+    int smaller;
+    int gretter;
+    System.out.println("Enter how numbers you want to input");
+    Scanner sc = new Scanner(System.in);
+    input=sc.nextInt();
+    System.out.println("you have to enter "+input+" numbers");
+    gretter=0;
+    for(int i=1;i<=input;i++){
+      smaller=sc.nextInt();
+      gretter=(smaller>gretter)?smaller:gretter;
+      
+    }
+    Sysrem.out.println("Greatest number "+gretter); 
+  }
+}
