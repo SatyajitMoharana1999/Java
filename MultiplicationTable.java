46ea3f429c900912c7ef315ef43f80ee17f18b30
Satyajit Moharana <117591813+SatyajitMoharana1999@users.noreply.github.com>
2023-01-10 10:03:01 +0530

    Update MultiplicationTable.java

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -3,8 +3,9 @@ public class MultiplicationTable{
   public static void main(String args[]){
     Scanner sc=new Scanner(System.in);
     int input;
-    int i;
+    int i=1;
     System.out.println("Enter the number whose multiplication table you want to see");
+    input=sc.nextInt();
     while(i<=10){
       System.out.println(input+" * "+i+" = "+input*i);
       i++;
