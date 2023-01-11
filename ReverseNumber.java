97b05b77822869d58d261418fd426e18fabfa104
Satyajit Moharana <117591813+SatyajitMoharana1999@users.noreply.github.com>
2023-01-11 13:37:14 +0530

    Update ReverseNumber.java

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 public class ReverseNumber{
   public static void main(String args[]){
     Scanner sc=new Scanner(System.in);
-    System.out.println("Enter any number);
+    System.out.println("Enter any number");
     int input=sc.nextInt();
     int reminder;
     int reverse=0;
     
     while(input>0){
-      reminder=input/10;
+      reminder=input%10;
       reverse=reverse*10+reminder;
       input=input/10;
     }
