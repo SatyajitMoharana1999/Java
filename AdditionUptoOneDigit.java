a9aa480c6490d1c8dedd5cddadaf3a709f448a03
Satyajit Moharana <117591813+SatyajitMoharana1999@users.noreply.github.com>
2023-01-14 13:21:54 +0530

    Create AdditionUptoOneDigit.java

---
+++ AdditionUptoOneDigit.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class AdditionUptoOneDigit{
+  public static void main(String args[]){
+    Scanner s=new Scanner(System.in);
+    int number=s.nextInt();
+    int sum;
+    for(int i=1;number>=10;i++){
+      sum=0;
+      for(int j=number;j>0;j=j/10){
+        sum=sum+j%10;
+      }
+      number=sum;
+    }
+    System.out.println("Addition up to one digit : "+number);
+  }
+}
