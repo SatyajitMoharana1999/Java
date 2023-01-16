ab54064d7eb0dab71cbe36761737b30f7005f341
Satyajit Moharana <117591813+SatyajitMoharana1999@users.noreply.github.com>
2023-01-16 16:47:03 +0530

    Update StarPattern_1.java

--- StarPattern_1.java
+++ StarPattern_1.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class StarPattern_1{
   public static void main(String args[]){
     Scanner sc=new Scanner(System.in);
+    System.out.println("Enter");
     int n=sc.nextInt();
     for(int i=1;i<=n;i++){
       for(int j=1;j<=i;j++){
@@ -11,3 +12,14 @@ public class StarPattern_1{
     }
   }
 }
+
+
+*/--
+    input :5
+    output: 
+            1
+            12
+            123
+            1234
+            12345
+      --/*
