75984475ec6ac9626a1a6c0f55f920d580b97357
Satyajit Moharana <117591813+SatyajitMoharana1999@users.noreply.github.com>
2023-01-22 23:48:45 +0530

    Create HCF_of_2_number.java

---
+++ HCF_of_2_number.java
@@ -0,0 +1,14 @@
+import java.util.Scannrr;
+public class HCF_of_2_number{
+  public static void main(String a[]){
+    Scanner s=new Scanner(System.in);
+    System.out.println("Enter the first number");
+    int a=s.nextInt();
+    System.out.println("Enter second number");
+    int b=s.nextInt();
+    int sml=a<b?a:b; //sml for storing small number 
+    for(int i=sml;i>=1;i--){
+      
+    }
+  }
+}
