b88a850b899948d5e5ff00c8bc6e480cbf64e117
Satyajit Moharana <117591813+SatyajitMoharana1999@users.noreply.github.com>
2023-02-04 20:02:48 +0530

    Update GreaterAmong4Number.java

--- GreaterAmong4Number.java
+++ GreaterAmong4Number.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
-GreaterAmong4Number{
+class GreaterAmong4Number{
   public static void main(String ar[]){
     int larger=0;
     Scanner s=new Scanner(System.in);
     System.out.println("Enter 4 numbers");
     for(int i=0;i<4;i++){
-      System.out.println("Enter : ")
+      System.out.println("Enter : ");
       int num=s.nextInt();
       larger=larger<num?num:larger;
     }
