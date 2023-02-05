e0e88b356aa15810994124b2cf76652f69cd46f5
Satyajit Moharana <117591813+SatyajitMoharana1999@users.noreply.github.com>
2023-02-05 10:46:16 +0530

    Create StarPattern_3.java

---
+++ StarPattern_3.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner; 
+class StarPattern_3{
+  public static void main(String arg[]){
+    Scanner s=new Scanner(System.in);
+    System.out.println("Enter any number");
+    int n=s.nextInt();
+    for(int i=1;i<=n;i++){
+      for(int j=1;j<=n-i;i++){
+        System.out.print(" ");
+        //loop for creating spaces
+      }
+      for(int k=1;k<=2*i-1;k++){
+        System.out.print("*");
+        //loop for creating odd number of stars in each line in increasing order
+      }
+      System.out.println();
+    }
+  }
+}
